2a6fa0fa9f960454cd8991a439ff45e2fcdc43f4
GitMats85 <107111236+GitMats85@users.noreply.github.com>
2024-03-18 23:09:24 +0100

    Started animations

--- Assets/Scripts/Generic/PlayerMovement.cs
+++ Assets/Scripts/Generic/PlayerMovement.cs
@@ -11,12 +11,24 @@ public class PlayerMovement : MonoBehaviour
     public ContactFilter2D groundFilter;
     private Vector2 moveInput;
     private Rigidbody2D rb;
+    Animator animator;
+
     private bool isGrounded => rb.IsTouching(groundFilter);
     private bool shouldJump;
+    private string currentState;
+
+    //Animation states
+    const string PLAYER_IDLE = "P_Idle";
+    const string PLAYER_RUN = "P_Run";
+    const string PLAYER_ATTACK = "P_Attack";
+    const string PLAYER_DEAD = "P_Dead";
+    const string PLAYER_JUMP = "P_Jump";
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     public void SetStartPos(float newPosX, float newPosY)
@@ -36,7 +48,24 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        //isGrounded = rb.IsTouching(groundFilter);  
+        //isGrounded = rb.IsTouching(groundFilter);
+        
+        if (moveInput.Equals(Vector2.zero))
+        {
+            ChangeAnimationState(PLAYER_IDLE);
+        }
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+        //Stoppa samma animation från att avbryta sig själv
+        if (currentState == newState) return;
+
+        //Spela animationen
+        animator.Play(newState);
+
+        //Uppdatera den nuvarande animationen
+        currentState = newState;
     }
 
     void FixedUpdate()
